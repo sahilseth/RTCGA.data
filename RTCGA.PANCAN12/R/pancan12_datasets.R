324293b866d87d24029690b17295fc35963b53ec
pbiecek <przemyslaw.biecek@gmail.com>
2015-06-17 02:06:06 +0200

    man skeleton

---
+++ RTCGA.PANCAN12/R/pancan12_datasets.R
@@ -0,0 +1,28 @@
+#' 
+#' PANCAN12 datasets from Cancer Browser project
+#'
+#' Package provides cnv datasets from Cancer Browser for 12 cohorts types 
+#'
+#' @format NULL
+#' @source \url{https://genome-cancer.ucsc.edu/proj/site/hgHeatmap/}
+#' 
+#' @name pancan12
+#' @rdname pancan12
+#' @aliases cnv.cb, mutation.cb, expression.cb, clinical.cb
+#'
+"clinical.cb"
+#' @name pancan12
+#' @rdname pancan12
+#' @format NULL
+"cnv.cb"
+#' @name pancan12
+#' @rdname pancan12
+#' @format NULL
+"mutation.cb"
+#' @name pancan12
+#' @rdname pancan12
+#' @format NULL
+"expression.cb"
+#' @name pancan12
+#' @rdname pancan12
+#' @format NULL
